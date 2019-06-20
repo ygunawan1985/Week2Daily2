c1ea80395338dea8158903bf70995640abb89476
ygunawan1985 <yohanes.gunawan@mobileappscompany.us>
2019-06-19 22:21:54 -0400

    Build 1

--- app/src/main/java/com/example/week1daily2/QuestionFivePrintMultiplicationTable.java
+++ app/src/main/java/com/example/week1daily2/QuestionFivePrintMultiplicationTable.java
@@ -9,12 +9,23 @@ public class QuestionFivePrintMultiplicationTable {
     }
 
     public void printTables(){
+
+        int[][] array = new int[10][10];
+
         for(int i = 1; i <= 10; i++){
-            for(int j = 1; j <= 10; j++){
-                System.out.print((i*j) + " ");
+            for(int j = 1; j <= 10; j++) {
+                array[i - 1][j - 1] = i * j;
+            }
+        }
+
+        for(int i = 0; i < 10; i++){
+            for(int j = 0; j < 10; j++){
+                System.out.print(array[i][j] + " ");
             }
             System.out.println();
         }
+
+
     }
 
 }
